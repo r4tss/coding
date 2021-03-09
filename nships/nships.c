128b46380f73e916a116ce8163653de15ae82891
r4tss <68610647+r4tss@users.noreply.github.com>
2021-03-09 01:23:18 +0100

    Made new custom menu function for UI

--- nships/nships.c
+++ nships/nships.c
@@ -1,4 +1,9 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define height 	80
+#define width	200
 
 struct p {
 	int x;
@@ -14,4 +19,57 @@ struct p rP() {
 	}
 	p.y = (rand() % (height - 2)) + 1;
 	return p;
+}
+
+struct items {
+        int len;
+        char items[3][15];
+};
+
+int menu(int y, int x, int h, int w, int aLen, char it[][15])
+{
+        int key, i, c = 0, o = 0;
+        WINDOW * menu = newwin(h, w, y, x);
+        refresh();
+        box(menu, 0, 0);
+        while(o == 0)
+        {
+                for(i = 0; i < aLen; ++i) {
+                        if(i == c) {
+                                wattron(menu, A_STANDOUT);
+                                mvwprintw(menu, i + 1, 1, "%s", it[i]);
+                                wattroff(menu, A_STANDOUT);
+                        } else {
+                                mvwprintw(menu, i + 1, 1, "%s", it[i]);
+                        }
+                }
+                switch(key = wgetch(menu))
+                {
+                        case 119:
+                                if(c > 0)
+                                        --c;
+                                break;
+                        case 115:
+                                if(c < aLen)
+                                        ++c;
+                                break;
+                        case 10:
+                                o = 1;
+                                break;
+                        case 27:
+                                o = 2;
+                                break;
+                }
+                wrefresh(menu);
+        }
+        delwin(menu);
+        if(o == 1)
+                return c;
+        else
+                return -1;
+}
+
+int main() 
+{
+	
 }
--- nsnake/nsnake.c
+++ nsnake/nsnake.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <unistd.h>
 
+#define height	50
+#define width	100
+
 struct p {
 	int x;
 	int y;
@@ -14,8 +17,6 @@ struct s {
 	int len;
 };
 
-int height = 50;
-int width = 100;
 
 struct p rP() {
 	struct p p;
@@ -35,13 +36,64 @@ bool endGame(WINDOW * win) {
 	wgetch(win);
 }
 
+struct items {
+        int len;
+        char items[3][15];
+};
+
+int menu(int y, int x, int h, int w, int aLen, char it[][15])
+{
+        int key, i, c = 0, o = 0;
+        WINDOW * menu = newwin(h, w, y, x);
+        refresh();
+        box(menu, 0, 0);
+        while(o == 0)
+        {
+                for(i = 0; i < aLen; ++i) {
+                        if(i == c) {
+                                wattron(menu, A_STANDOUT);
+                                mvwprintw(menu, i + 1, 1, "%s", it[i]);
+                                wattroff(menu, A_STANDOUT);
+                        } else {
+                                mvwprintw(menu, i + 1, 1, "%s", it[i]);
+                        }
+                }
+                switch(key = wgetch(menu))
+                {
+                        case 119:
+                                if(c > 0)
+                                        --c;
+                                break;
+                        case 115:
+                                if(c < aLen)
+                                        ++c;
+                                break;
+                        case 10:
+                                o = 1;
+                                break;
+                        case 27:
+                                o = 2;
+                                break;
+			default:
+				break;
+                }
+                wrefresh(menu);
+        }
+        delwin(menu);
+        if(o == 1)
+                return c;
+        else
+                return 1;
+}
+
 int main()
 {
 	srand(time(0));
 	struct p candy = rP();
 	struct p o = {width / 2, height / 2};
 	struct s s = { o, 0, 1}; //dir is 0 = down, 1 = up, 2 = left, 3 = right.
-	int key, n, i, a = 0, time = 200000, score = 0;
+	struct items sit = {2, {"Play Game", "Exit"}};
+	int key, n, i, a = 0, time = 200000, score = 0, state = 0;
 	struct p pray[60];
 	bool exit = false;
 	
@@ -49,76 +101,93 @@ int main()
 	int sx = (COLS - width) / 2, sy = (LINES - height) / 2;
 	curs_set(0);
 	noecho();
+	keypad(stdscr, TRUE);
 	WINDOW * win = newwin(height, width, sy, sx);
-	nodelay(win, 1);
 	refresh();
 
 	while(!exit)
 	{
-			switch(key = wgetch(win))
-			{
-				case 115: //W go up
-					if( s.dir == 2 || s.dir == 3) { s.dir = 0; }
-					break;
-				case 119: //S go down
-					if( s.dir == 2 || s.dir == 3) { s.dir = 1; }
-					break;
-				case 97: //A go left
-					if(s.dir == 0 || s.dir == 1) { s.dir = 2; }
-					break;
-				case 100: //D go right
-					if(s.dir == 0 || s.dir == 1) { s.dir = 3; }
-					break;
-				case 27: //ESC
+		switch(state)
+		{
+			case 0:	//Start menu state
+				nodelay(win, 0);
+				mvwprintw(win, (height / 2) - 10, (width / 2) - 15, "nsnake");
+				wrefresh(win);
+				state = menu((LINES - 15) / 2, (COLS - 10) / 2, 10, 15, sit.len, sit.items);
+				if(state == 0) {
+					state = 1;
+				} else if(state == 1) {
 					exit = true;
-				default:
-					break;
-			}
-			switch(s.dir)
-			{
-				case 0:
-					s.p.y++;
-					break;
-				case 1:
-					s.p.y--;
-					break;
-				case 2:
-					s.p.x-=2;
-					break;
-				case 3:
-					s.p.x+=2;
-					break;
-			}
-			if (s.p.y >= height-1 || s.p.y <= 0 || s.p.x >= width-1 || s.p.x <= 0) { endGame(win); exit = true; }
-			for (i = 2; i < s.len + 1; i++) {
-				if(s.p.x == pray[i].x && s.p.y == pray[i].y) {
-					endGame(win); exit = true;
-					break;
 				}
-			}
-		//	if (s.p.y == height-1) { s.p.y = 1; }
-		//	if (s.p.y == 0) { s.p.y = height-1;}
-		//	if (s.p.x >= width-1) { s.p.x = 2; }
-		//	if (s.p.x <= 0) { s.p.x = width-2; }
-			if (s.p.x == candy.x && s.p.y == candy.y) {
-				candy = rP();
-				score += 100;
-				a += 1000;
-				time = time - score * 5;
-				s.len++;
-			}
-			wclear(win);
-			for ( n = s.len; n > 0; n--) {
-				pray[0] = s.p;
-				pray[n] = pray[n-1];
-				mvwprintw(win, pray[n+1].y, pray[n+1].x, "+");
-			}
-			mvwprintw(win, s.p.y, s.p.x, "@");
-			mvwprintw(win, candy.y, candy.x, "#");
-			box(win, 0, 0);
-			mvwprintw(win, 0, 45, " Score: %i ", score);
-			wrefresh(win);
-			usleep(time);
+				break;
+			case 1: //Play state
+				nodelay(win, 1);
+				switch(key = wgetch(win))
+				{
+					case 115: //W go up
+						if( s.dir == 2 || s.dir == 3) { s.dir = 0; }
+						break;
+					case 119: //S go down
+						if( s.dir == 2 || s.dir == 3) { s.dir = 1; }
+						break;
+					case 97: //A go left
+						if(s.dir == 0 || s.dir == 1) { s.dir = 2; }
+						break;
+					case 100: //D go right
+						if(s.dir == 0 || s.dir == 1) { s.dir = 3; }
+						break;
+					case 27: //ESC
+						state = 0;
+					default:
+						break;
+				}
+				switch(s.dir)
+				{
+					case 0:
+						s.p.y++;
+						break;
+					case 1:
+						s.p.y--;
+						break;
+					case 2:
+						s.p.x-=2;
+						break;
+					case 3:
+						s.p.x+=2;
+						break;
+				}
+				if (s.p.y >= height-1 || s.p.y <= 0 || s.p.x >= width-1 || s.p.x <= 0) { endGame(win); exit = true; }
+				for (i = 2; i < s.len + 1; i++) {
+					if(s.p.x == pray[i].x && s.p.y == pray[i].y) {
+						endGame(win); exit = true;
+						break;
+					}
+				}
+			//	if (s.p.y == height-1) { s.p.y = 1; }
+			//	if (s.p.y == 0) { s.p.y = height-1;}
+			//	if (s.p.x >= width-1) { s.p.x = 2; }
+			//	if (s.p.x <= 0) { s.p.x = width-2; }
+				if (s.p.x == candy.x && s.p.y == candy.y) {
+					candy = rP();
+					score += 100;
+					a += 1000;
+					time = time - score * 5;
+					s.len++;
+				}
+				wclear(win);
+				for ( n = s.len; n > 0; n--) {
+					pray[0] = s.p;
+					pray[n] = pray[n-1];
+					mvwprintw(win, pray[n+1].y, pray[n+1].x, "+");
+				}
+				mvwprintw(win, s.p.y, s.p.x, "@");
+				mvwprintw(win, candy.y, candy.x, "#");
+				box(win, 0, 0);
+				mvwprintw(win, 0, 45, " Score: %i ", score);
+				wrefresh(win);
+				usleep(time);
+				break;
+		}
 	}
 	endwin();
 	return 0;

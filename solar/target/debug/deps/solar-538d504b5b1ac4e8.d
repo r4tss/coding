cd403901b2f7cafe1571bdac5079ba5fac42a82c
r4tss <68610647+r4tss@users.noreply.github.com>
2022-01-26 00:01:42 +0100

    Started new project

--- solar/target/debug/deps/solar-538d504b5b1ac4e8.d
+++
@@ -1,5 +0,0 @@
-/home/ed/coding/solar/target/debug/deps/solar-538d504b5b1ac4e8: src/main.rs
-
-/home/ed/coding/solar/target/debug/deps/solar-538d504b5b1ac4e8.d: src/main.rs
-
-src/main.rs:
--- solar/target/debug/solar.d
+++
@@ -1,1 +0,0 @@
-/home/ed/coding/solar/target/debug/solar: /home/ed/coding/solar/src/main.rs

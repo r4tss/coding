78fecbab4d1c3c4c01699e5065fca89aa4a7d74b
r4tss <68610647+r4tss@users.noreply.github.com>
2022-01-27 14:14:41 +0100

    Added curses like library to solar

--- solar/src/main.rs
+++ solar/src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
 const G: f32 = (10^-11) as f32 * 6.674;
 const PI: f32 = 3.14159;
 
@@ -10,12 +13,15 @@ struct Body
     mass: f32,
 }
 
-fn main()
+fn main() -> Result<(), io::Error>
 {
-    // TODO: Find a suitable graphics library
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut term = Terminal::new(backend)?;
     let mut Bodies: Vec<Body> = vec![];
 
     let earth = Body { x: 1.0, y: 2.0, x_vel: 0.0, y_vel: 0.0, mass: 30.0};
+    Bodies.push(earth);
 
     for i in 1..Bodies.len() // Iterate through bodies and apply gravitational pull for all bodies
     {
@@ -25,4 +31,6 @@ fn main()
     }
 
     println!("length: {}, last x: {}", Bodies.len(), Bodies[Bodies.len() - 1].x);
+
+    Ok(())
 }
